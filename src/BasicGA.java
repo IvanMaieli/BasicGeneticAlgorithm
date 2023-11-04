2b0aacdc2b554cd7c944bf5565255ec7df1bb592
Ivan <67658275+IvanMaieli@users.noreply.github.com>
2023-11-04 21:47:28 +0100

    Init population

--- src/BasicGA.java
+++ src/BasicGA.java
@@ -4,7 +4,7 @@ public class BasicGA {
     public static void main(String[] args) {
         //Instanziare l'algoritmo genetico
         GeneticAlgorithm ga = new GeneticAlgorithm(100, .01, .95, 0);
-
+        Population population = ga.initPopulation(46);
     }
 
 }
