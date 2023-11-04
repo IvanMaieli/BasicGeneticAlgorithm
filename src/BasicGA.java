084db9b80a2686dddd0fdffb37fb31575dcc09f3
Ivan <67658275+IvanMaieli@users.noreply.github.com>
2023-11-04 18:30:20 +0100

    qui sto togliendo un commento

--- src/BasicGA.java
+++ src/BasicGA.java
@@ -4,7 +4,6 @@ public class BasicGA {
     public static void main(String[] args) {
         //Instanziare l'algoritmo genetico
         GeneticAlgorithm ga = new GeneticAlgorithm(100, .01, .95, 0);
-        //ciao come va
     }
 
 }
