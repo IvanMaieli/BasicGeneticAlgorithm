ef319b0d54e28cd948b860b2c2fbc00dbb96deaf
Ivan <67658275+IvanMaieli@users.noreply.github.com>
2023-11-04 16:40:26 +0100

    Genetic Algo class Uploading

---
+++ src/GeneticAlgorithm.java
@@ -0,0 +1,16 @@
+package src;
+
+public class GeneticAlgorithm {
+    private int populationSize;
+    private double mutationRate;
+    private double crossoverRate;
+    private int elitismCount;
+    
+    public GeneticAlgorithm(int populationSize, double mutationRate, double crossoverRate, 
+                            int elitismCount) {
+        this.populationSize = populationSize;
+        this.mutationRate = mutationRate;
+        this.crossoverRate = crossoverRate;
+        this.elitismCount = elitismCount;
+    }
+}
